896c4a62e0fa97a9da84fbfd9e223f1be2233a4d
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2024-02-05 22:10:41 +0100

    Agregar archivos de proyecto.

---
+++ Grains/HelloGrain.cs
@@ -0,0 +1,31 @@
+﻿using GrainsInterfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class HelloGrain : Grain, IHello
+    {
+        private readonly ILogger _logger;
+
+        public HelloGrain(ILogger<HelloGrain> logger)
+        {
+            _logger = logger;
+        }
+
+        public ValueTask<string> SayHello(string greeting)
+        {
+            _logger.LogInformation(
+            "SayHello message received: greeting = '{Greeting}'", greeting);
+
+            return ValueTask.FromResult(
+                $"""
+            Client said: '{greeting}', so HelloGrain says: Hello!
+            """);
+        }
+    }
+}
---
+++ GrainsInterfaces/IHello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrainsInterfaces
+{
+    public interface IHello : IGrainWithIntegerKey
+    {
+        ValueTask<string> SayHello(string greeting);
+    }
+}
